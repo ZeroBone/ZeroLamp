a47b16c19b61918a288d544d097bb33e18e9769a
Alexander Mayorov <zerobone21@gmail.com>
2024-12-11 15:01:27 +0100

    Improved visualization of time when it is unavailable

--- zerolamp/fg_clock.cpp
+++ zerolamp/fg_clock.cpp
@@ -17,20 +17,26 @@ void ClockFG::render_frame(int offset_x, int offset_y, int viewport_width, int v
 
   assert(viewport_width <= logical_width);
   assert(viewport_height <= logical_height);
-  
-  struct tm* cur_time = get_current_time();
-
-  if (cur_time == nullptr) {
-    draw_character(offset_x + 1, offset_y + 4, '?', offset_x, offset_x + viewport_width - 1, CRGB::Red);
-    return;
-  }
 
   // extract hours and minutes as strings
   char hourStr[3];  // 2 digits + null terminator
   char minuteStr[3]; // 2 digits + null terminator
 
-  snprintf(hourStr, sizeof(hourStr), "%02d", cur_time->tm_hour);
-  snprintf(minuteStr, sizeof(minuteStr), "%02d", cur_time->tm_min);
+  struct tm* cur_time = get_current_time();
+
+  if (cur_time == nullptr) {
+    // time is not available at the moment
+    hourStr[0] = '?';
+    hourStr[1] = '?';
+    hourStr[2] = '\0';
+    minuteStr[0] = '?';
+    minuteStr[1] = '?';
+    minuteStr[2] = '\0';
+  }
+  else {
+    snprintf(hourStr, sizeof(hourStr), "%02d", cur_time->tm_hour);
+    snprintf(minuteStr, sizeof(minuteStr), "%02d", cur_time->tm_min);
+  }
 
   draw_string_horizontally(offset_x, 2, hourStr, offset_x, offset_x + viewport_width - 1, CRGB::Green, 0);
   draw_string_horizontally(offset_x, 9, minuteStr, offset_x, offset_x + viewport_width - 1, CRGB::Blue, 0);
